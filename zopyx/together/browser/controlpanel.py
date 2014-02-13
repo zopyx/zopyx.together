e7ce267f695e4e730e0715bc18a468285c560767
zopyx <yet@gmx.de>
2014-02-13 18:30:03 +0100

    updated

---
+++ zopyx/together/browser/controlpanel.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from plone.app.registry.browser import controlpanel
+
+from zopyx.together.interfaces import ITogetherSettings
+from zopyx.together import MessageFactory as _
+
+
+class ETogetherSettingsEditForm(controlpanel.RegistryEditForm):
+
+    schema = ITogetherSettings
+    label = _(u'zopyx.together settings')
+    description = _(u'')
+
+    def updateFields(self):
+        super(TogetherSettingsEditForm, self).updateFields()
+
+    def updateWidgets(self):
+        super(TogetherSettingsEditForm, self).updateWidgets()
+
+
+class TogetherSettingsControlPanel(controlpanel.ControlPanelFormWrapper):
+    form = TogetherSettingsEditForm
